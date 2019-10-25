f940a984b12460754f9e2cdd605e891809144089
imshenzhuo <im.shenzhuo@gmail.com>
2019-10-25 18:10:20 +0800

    cpp bst

--- tree/BinarySearchTree.cpp
+++ tree/BinarySearchTree.cpp
@@ -2,70 +2,22 @@
 #include <random>
 #include "BinarySearchTree.h"
 
-TreeNode& TreeNode::operator=(const TreeNode& rhs)
-{
-    if (left) {
-        delete left;
-        left = nullptr;
-    }
-    if (right) {
-        delete right;
-        right = nullptr;
-    }
-    value = rhs.value;
-    left = rhs.left;
-    right = rhs.right;
-    return *this;
-}
-BinStrTree& BinStrTree::operator=(const BinStrTree& bst)
-{
-    TreeNode* new_root = new TreeNode(*bst.root);
-    delete root;
-    root = new_root;
-    return *this;
-}
-
-bool BinStrTree::insert(int v) {
-    if (!root)  return false;
-    return root->insert(v);
-}
-
-TreeNode *TreeNode::insert(int v) {
-
-    if (v == value) {
-        return nullptr;
-    }
-    if (v > value)  {
-        if(!right)  right = new TreeNode(v);
-        else right = right->insert(v);
-    } else {
-        if(!left)  left = new TreeNode(v);
-        else left = left->insert(v);
-    }
-    return this;
-}
-
-void TreeNode::printTree() {
-    if (left)   left->printTree();
-    std::cout << value << "   " << std::endl;
-    if (right)  right->printTree();
-}
-
 int main(int argc, char const *argv[])
 {
-    const int N = 500000;
+
+    const int N = 5;
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(1,600); // distribution in range [1, 6]
 
-    std::cout << dist6(rng) << std::endl;
-    int a = 4;
-    BinStrTree bst(4);
+    BinarySearchTree<int> *bst = new BinarySearchTree<int>;
     /* code */
     for (size_t i = 0; i < N; i++)
     {
-        bst.insert(dist6(rng));
+        bst->insert(dist6(rng));
     }
-    bst.printTree();
+    bst->printTree();
+    delete bst;
+    bst = nullptr;
     return 0;
 }
--- tree/BinarySearchTree.h
+++ tree/BinarySearchTree.h
@@ -1,49 +1,120 @@
+/**
+ * c++实现二叉搜索树 
+ * 
+ * 
+ */ 
+
 #include <iostream>
 #include <string> 
-using std::string;
+using namespace std;
 
-class TreeNode {
-private:
-    int value;
-    TreeNode *left;
-    TreeNode *right;
-public:
-    TreeNode(int v)
-        : value(v), left(nullptr), right(nullptr)
-    { }
-    TreeNode(const TreeNode& rhs):
-        value(rhs.value), left(rhs.left), right(rhs.right) {
-    }
-    TreeNode& operator=(const TreeNode& rhs);
-    ~TreeNode() {
-        if (left) {
-            delete left;
-            left = nullptr;
-        }
-        if (right) {
-            delete right;
-            right = nullptr;
-        }
-    }
-    int getValue() {
-        return value;
-    }
-    TreeNode *insert(int v);
-    void printTree();
-};
-
-class BinStrTree
+template <typename Comparable>
+class BinarySearchTree
 {
-private:
-    TreeNode *root;
 public:
-    BinStrTree(int v): root(new TreeNode(v)) {}
-    BinStrTree(const BinStrTree& bst):
-        root(new TreeNode(*bst.root)){};
-    BinStrTree& operator=(const BinStrTree& bst);
-    ~BinStrTree() { delete root;}
+    BinarySearchTree() {}
+    BinarySearchTree(const BinarySearchTree& bst);
+    const BinarySearchTree& operator=(const BinarySearchTree& bst);
+    ~BinarySearchTree(){
+        if(root) {
+            cout << "delete tree "  << endl;
+            delete root;
+            root = nullptr;
+        }
+    };
 
-    void printTree() { root->printTree(); }
-    bool insert(int);
+    const Comparable & findMin() const {
+        findMin(root);
+    }
+    const Comparable & findMax() const{
+        findMax(root);
+    }
+    bool contains( const Comparable & x) const {
+        return contains(x, root);
+    }
+    bool isEmpty() const;
+    void printTree() const {
+        printTree(root);
+    }
+
+    void makeEmpty();
+    void insert( const Comparable & x) {
+        insert(x, root);
+    }
+    void remove( const Comparable & x) {
+        remove(x, root);
+    }
+
+
+private:
     
+    struct BinaryNode
+    {
+        Comparable element;
+        BinaryNode *left;
+        BinaryNode *right;
+        int N;
+
+        BinaryNode(const Comparable & theElement, BinaryNode *lt, BinaryNode *rt)
+            : element(theElement), left(lt), right(rt) {  }   
+        ~BinaryNode() {
+            cout << "delete node " << this->element << endl;
+            if(left) {
+                delete left;
+                left = nullptr;
+            }
+            if (right) {
+                delete right;
+                right = nullptr;
+            }
+        }
+    };
+    
+    BinaryNode *root;
+    
+    void insert(const Comparable & x, BinaryNode * &t) const {
+        if (!t) t = new BinaryNode(x, NULL, NULL);
+        else if( x < t->element)    insert(x, t->left);
+        else if(t->element < x)     insert(x, t->right);
+    }
+    void remove(const Comparable & x, BinaryNode * &t) const {
+        if (!t) return;
+        if (x < t->element) remove(x, t->left);
+        else if(x > t->element) remove(t, t->right);
+        else if(!(t->left)&&!(t->right)) {
+            t->element = findMin(t->right)->element;
+            remove(t->element, t->right);
+        }else {
+            BinaryNode *oldNode = t;
+            t = (t->left!=NULL) ? t->left : t->right;
+            delete oldNode;
+        }
+    }
+    BinaryNode * findMin(BinaryNode *t) const {
+        if (t == NULL)  return NULL;
+        if (t->left == NULL)    return t;
+        return findMin(t->left);
+    }
+    BinaryNode * findMax(BinaryNode *t) const {
+        if (t == NULL)  return NULL;
+        if (t->right == NULL)    return t;
+        return findMin(t->right);
+    }
+    bool contains( const Comparable & x, BinaryNode *t) const {
+        if (x == NULL)  return false;
+        if (x < t->element) return contains(x, t->left);
+        if (x > t->element) return contains(x, t->right);
+        if (x == t->element) return true;
+    }
+    void makeEmpty(BinaryNode *&t);
+    void printTree(BinaryNode *t) const {
+        if (!t) return;
+        if (t->left)    printTree(t->left);
+        cout << t->element << endl;
+        if (t->right)   printTree(t->right);
+    }
+    BinaryNode *clone(BinaryNode *t) const;
 };
+
+
+// diff null nullptr
